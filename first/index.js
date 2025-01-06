1ad24435af4df27508fdbf2621ad826ea4c100db
aryankumar07 <ak2839426@gmail.com>
2025-01-06 13:02:27 +0530

    basic setup

---
+++ first/index.js
@@ -0,0 +1,13 @@
+const head1 = React.createElement("h1",{},"hello heading 1");
+const head2 = React.createElement("div",{},
+	[
+		React.createElement("h2",{},"inside first"),
+		React.createElement("h2",{},"inside first.second"),
+
+	]
+);
+const root1 = ReactDOM.createRoot(document.getElementById("root"))
+const root2 = ReactDOM.createRoot(document.getElementById("root1"))
+root1.render(head1);
+root2.render(head2);
+
