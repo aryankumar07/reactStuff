8b41e0a0393b5710d1c523e432f795a65e0be792
aryankumar07 <ak2839426@gmail.com>
2025-01-10 23:46:38 +0530

    update():basic Setup

--- first/app.js
+++ first/app.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const head1 = React.createElement("h1",{},"hello heading 1");
-const head2 = React.createElement("div",{},
-	[
-		React.createElement("h2",{},"inside first"),
-		React.createElement("h2",{},"inside first.second"),
 
-	]
-);
-const root1 = ReactDOM.createRoot(document.getElementById("root"))
-const root2 = ReactDOM.createRoot(document.getElementById("root1"))
-root1.render(head1);
-root2.render(head2);
+// React Element
+const title = <h1>Title heading</h1>
+
+// React Component
+const Heading = ()=>{
+	return <div>
+		{title}
+		<h1>This is a Heading</h1>
+	</div>
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<Heading/>)
 
